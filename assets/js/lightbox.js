ea8cab9cbd20ba6bebc650b35b676b9a8f280d33
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-06-13 12:07:50 +0200

    feat: add full screen

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -20,6 +20,38 @@ if (gallery) {
     errorMsg: params.errorMsg,
   });
 
+  lightbox.on("uiRegister", () => {
+    lightbox.pswp.ui.registerElement({
+      name: "fullscreen-button",
+      order: 15,
+      isButton: true,
+      html: {
+        isCustomSVG: true,
+        inner: '<path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3" id="pswp__icn-fullscreen-enter"/><path hidden id="pswp__icn-fullscreen-exit" d="M8 3v3a2 2 0 0 1-2 2H3m18 0h-3a2 2 0 0 1-2-2V3m0 18v-3a2 2 0 0 1 2-2h3M3 16h3a2 2 0 0 1 2 2v3"/>',
+        outlineID: "pswp__icn-fullscreen-enter",
+      },
+      title: "Toggle fullscreen",
+      onInit: (el, pswp) => {
+        const enterIcon = el.querySelector("#pswp__icn-fullscreen-enter");
+        const exitIcon = el.querySelector("#pswp__icn-fullscreen-exit");
+        const updateIcon = () => {
+          const isFullscreen = !!document.fullscreenElement;
+          enterIcon.hidden = isFullscreen;
+          exitIcon.hidden = !isFullscreen;
+        };
+        document.addEventListener("fullscreenchange", updateIcon);
+        pswp.on("close", () => document.removeEventListener("fullscreenchange", updateIcon));
+        el.addEventListener("click", () => {
+          if (document.fullscreenElement) {
+            document.exitFullscreen();
+          } else {
+            document.documentElement.requestFullscreen();
+          }
+        });
+      },
+    });
+  });
+
   if (params.enableDownload) {
     lightbox.on("uiRegister", () => {
       lightbox.pswp.ui.registerElement({
